1387d1f4ad982bbee845e39ac0266fe69323fd3d
Bastian Schattenberg <git@bestyan.de>
2020-06-05 03:43:15 +0200

    add localstoragedb
    broken right now because of forbidden characters in column names

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,13 @@
 import XLSX from 'xlsx';
+import localStorageDB from 'localstoragedb';
 
+const TABLENAME = "buchungsdaten";
+const DB = new localStorageDB("meldeschein", localStorage);
+
+/**
+ * reads xls to json
+ * @param {Event} e 
+ */
 function handleFile(e) {
     let files = e.target.files,
         f = files[0];
@@ -11,13 +19,64 @@ function handleFile(e) {
             cellDates: true
         });
 
-        /* DO SOMETHING WITH workbook HERE */
-        //let sheet = workbook.Sheets[0]; //not sure this works
+        /*
+            TODO: modify column names to not contain forbidden characters
+        */
         let sheet = workbook.Sheets[workbook.SheetNames[0]];
+        console.log(sheet);
+
+
         let sheet_as_json = XLSX.utils.sheet_to_json(sheet);
-        console.log(sheet_as_json);
+        //console.log(sheet_as_json);
+
+        updateDB(sheet_as_json)
     };
     reader.readAsArrayBuffer(f);
+}
+
+/**
+ * 
+ * @param {JSON} rows 
+ */
+function updateDB(rows) {
+    initDB(rows);
+}
+
+/**
+ * sets up db for use
+ * @param {JSON} rows 
+ */
+function initDB(rows) {
+    /*
+        clear old data and create table
+    */
+    if (DB.tableExists(TABLENAME)) {
+        // delete old data
+        DB.dropTable(TABLENAME);
+    }
+    DB.createTableWithData(TABLENAME, rows);
+
+    /*
+        reformat column Kunde to Vorname, Nachname
+    */
+    DB.alterTable(TABLENAME, ["Vorname", "Nachname"], "");
+    DB.update(TABLENAME, {}, row => {
+        try {
+
+            let name = row.Kunde.split(",");
+            let vorname = name[1].trim(), nachname = name[0].trim();
+            row.Vorname = vorname;
+            row.Nachname = nachname;
+
+        } catch (exception) {
+
+            console.error(`bad data in column Kunde: ${row.Kunde}`);
+
+        }
+    });
+    DB.commit();
+
+    console.log(DB.queryAll(TABLENAME));
 }
 
 document.getElementById('upload').addEventListener('change', handleFile, false);
