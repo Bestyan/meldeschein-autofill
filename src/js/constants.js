ebe92ece1ca1e34c6b7c1504b2f834b65fbacb59
Bastian Schattenberg <git@bestyan.de>
2020-06-12 18:34:10 +0200

    refactor

---
+++ src/js/constants.js
@@ -0,0 +1,16 @@
+export default {
+
+    SEARCH_RESULT_DATE_FORMAT: {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    },
+    
+    STATUS_DATE_FORMAt: {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    }
+};
