58893f1645f54c1467b7ee5dc6c96bcb30b59652
Bastian Schattenberg <git@bestyan.de>
2020-06-20 18:50:06 +0200

    fix wlan voucher hopefully for the last time

--- src/js/content_scripts/fill_wlan_voucher.js
+++ src/js/content_scripts/fill_wlan_voucher.js
@@ -1,7 +1,8 @@
 console.log("fill_wlan_voucher loaded");
 
+const HOTSPOT_TEXT = "Hotspot:";
+
 const TEXT_TO_DATA_FIELD = {
-    "Hotspot:": "hotspot",
     "Voucher-Definition:": "gueltigkeit",
     "Anzahl:": "anzahl",
     "Drucken:": "drucken",
@@ -26,45 +27,65 @@ const GUELTIGKEIT_TO_VALUE = {
     "22 Tage": "REF_HotVou22Tage"
 };
 
+/*
+    the wlan voucher site works like this:
+    you select the hotspot (and trigger the change event)
+    the entire lower half of the site reloads (resetting all the input fields)
+    now you may set your input fields
+*/
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-        const tdTags = document.getElementsByTagName("td");
-        const data_fields = {};
-        const text_to_data_field = JSON.parse(JSON.stringify(TEXT_TO_DATA_FIELD));
-
-        // find input fields / selects, skip if no textNode as first child
-        [...tdTags].filter(_ => _.hasChildNodes() && _.childNodes[0].nodeType === Node.TEXT_NODE).forEach(
-            td => {
-                const textNode = td.childNodes[0];
-                for (const [text, data_field] of Object.entries(text_to_data_field)) {
-
-                    if (textNode.nodeValue.includes(text)) {
-                        let input_field = td.nextSibling.childNodes[0];
-                        data_fields[data_field] = input_field;
-                        delete text_to_data_field[text];
-                        break;
-                    }
-                }
-            }
-        );
-
         console.log(request.data);
 
-        // set value and trigger change event
-        data_fields.hotspot.value = HOTSPOT_TO_VALUE[request.data.hotspot];
-        const changeEvent_hotspot = document.createEvent("HTMLEvents");
-        changeEvent_hotspot.initEvent("change", false, true);
-        data_fields.hotspot.dispatchEvent(changeEvent_hotspot);
-        
-        // set value and trigger change event
-        data_fields.gueltigkeit.value = GUELTIGKEIT_TO_VALUE[request.data.gueltigkeit];
-        const changeEvent_gueltigkeit = document.createEvent("HTMLEvents");
-        changeEvent_gueltigkeit.initEvent("change", false, true);
-        data_fields.hotspot.dispatchEvent(changeEvent_gueltigkeit);
+        const tdTags = document.getElementsByTagName("td");
 
-        // just setting value is enough here
-        data_fields.anzahl.value = 2;
-        data_fields.drucken.checked = true;
-        data_fields.kommentar.value = request.data.kommentar;
+        // find input fields / selects, skip if no textNode as first child
+        [...tdTags].filter(_ => _.hasChildNodes() &&
+                _.childNodes[0].nodeType === Node.TEXT_NODE &&
+                _.childNodes[0].nodeValue.includes(HOTSPOT_TEXT))
+            .forEach(
+                td => {
+                    const hotspot = td.nextSibling.childNodes[0];
+
+                    // set value and trigger change event
+                    hotspot.value = HOTSPOT_TO_VALUE[request.data.hotspot];
+                    const changeEvent_hotspot = document.createEvent("HTMLEvents");
+                    changeEvent_hotspot.initEvent("change", false, true);
+                    hotspot.dispatchEvent(changeEvent_hotspot);
+                }
+            );
+
+        // wait 1s for the site's ajax reload
+        new Promise(resolve => setTimeout(resolve, 1000)).then(
+            () => {
+                const data_fields = {};
+                const text_to_data_field = JSON.parse(JSON.stringify(TEXT_TO_DATA_FIELD)); // copy the object
+                const tdTags_after_reload = document.getElementsByTagName("td");
+
+                [...tdTags_after_reload].filter(_ => _.hasChildNodes() && _.childNodes[0].nodeType === Node.TEXT_NODE).forEach(
+                    td => {
+                        const textNode = td.childNodes[0];
+                        for (const [text, data_field] of Object.entries(text_to_data_field)) {
+
+                            if (!textNode.nodeValue.includes(text)) {
+                                continue;
+                            }
+
+                            const input_field = td.nextSibling.childNodes[0];
+                            data_fields[data_field] = input_field;
+                            delete text_to_data_field[text];
+                            break;
+                        }
+                    }
+                );
+
+                // set values
+                data_fields.gueltigkeit.value = GUELTIGKEIT_TO_VALUE[request.data.gueltigkeit]
+                data_fields.anzahl.value = 2;
+                data_fields.drucken.checked = true;
+                data_fields.kommentar.value = request.data.kommentar;
+            }
+        )
+
     }
 );
